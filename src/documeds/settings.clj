1c4cf191ce5ff133fa94ce96ea1d1ab35f83dd9a
Dan McGrady <dan@dmix.ca>
2012-02-15 12:23:20 -0500

    redis

--- src/documeds/settings.clj
+++ src/documeds/settings.clj
@@ -1,5 +1,10 @@
 (ns documeds.settings)
 
-(def redis-url (get (System/getenv) "REDIS_URL"))
-(def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
-(def redis-pass (get (System/getenv) "REDIS_PASS"))
+;redis://redistogo:527d2a676998e2ccb850c25677d3cc8c@dogfish.redistogo.com:9583/
+; (def redis-url (get (System/getenv) "REDIS_URL"))
+; (def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
+; (def redis-pass (get (System/getenv) "REDIS_PASS"))
+
+(def redis-url "dogfish.redistogo.com")
+(def redis-pass "527d2a676998e2ccb850c25677d3cc8c")
+(def redis-port 9583)
