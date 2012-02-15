be3f05fa3e321e1890a10ce1abdba9fb0fb0897c
Dan McGrady <dan@dmix.ca>
2012-02-15 12:41:31 -0500

    reverting res

--- src/documeds/settings.clj
+++ src/documeds/settings.clj
@@ -1,10 +1,5 @@
 (ns documeds.settings)
 
-;redis://redistogo:527d2a676998e2ccb850c25677d3cc8c@dogfish.redistogo.com:9583/
-; (def redis-url (get (System/getenv) "REDIS_URL"))
-; (def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
-; (def redis-pass (get (System/getenv) "REDIS_PASS"))
-
-(def redis-url "dogfish.redistogo.com")
-(def redis-pass "527d2a676998e2ccb850c25677d3cc8c")
-(def redis-port 9583)
+(def redis-url (get (System/getenv) "REDIS_URL"))
+(def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
+(def redis-pass (get (System/getenv) "REDIS_PASS"))
