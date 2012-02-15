37acf2b4d127137ada06fded5ba769aed2aa3789
Dan McGrady <dan@dmix.ca>
2012-02-15 11:34:15 -0500

    convert port to int

--- src/documeds/settings.clj
+++ src/documeds/settings.clj
@@ -1,5 +1,5 @@
 (ns documeds.settings)
 
 (def redis-url (get (System/getenv) "REDIS_URL"))
-(def redis-port (get (System/getenv) "REDIS_PORT"))
+(def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
 (def redis-pass (get (System/getenv) "REDIS_PASS"))
