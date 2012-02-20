9cc6ae732ed0c59880d488ca84c125398345070c
Dan McGrady <dan@dmix.ca>
2012-02-20 15:22:17 -0500

    autocomplete indexing server

--- src/documeds/autocomplete/loader.clj
+++ src/documeds/autocomplete/loader.clj
@@ -4,8 +4,6 @@
         documeds.redis
         [documeds.models.medication :as medication]))
 
-(def temp (list {:id "503", :name "Mexiletine", :subtitle "subtitle"} {:id "422", :name "Triamcinolone Topical", :subtitle "subtitle"} {:id "341", :name "Fluoride", :subtitle "subtitle"} {:id "260", :name "Stool Softeners", :subtitle "subtitle"} {:id "504", :name "Thalidomide", :subtitle "subtitle"} {:id "423", :name "Saquinavir", :subtitle "subtitle"} {:id "342", :name "Probenecid", :subtitle "subtitle"} {:id "261", :name "Goserelin Implant", :subtitle "subtitle"} {:id "180", :name "Cimetidine", :subtitle "subtitle"} {:id "505", :name "Estradiol Topical", :subtitle "subtitle"} {:id "424", :name "Oxiconazole", :subtitle "subtitle"} {:id "343", :name "Dutasteride", :subtitle "subtitle"} {:id "50666", :name "Probase", :subtitle "subtitle"}))
-
 (defn store-index [prefix]
   "Store each prefix into master set"
   @(@r [:sadd key-autocomplete prefix]))
@@ -25,3 +23,8 @@
   "Iterate through all medications in the redis database and create index"
   (let [meds (medication/all)]
     (flatten (map create-index-for meds))))
+
+(defn -main [& args]
+  (println "Running indexer")
+  (store-indexes)
+  (medication/build-alpha-index))
