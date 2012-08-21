f3a8ceb435347a6dfe8147c6c3432c00e2e56dd3
Dan McGrady <dan@dmix.ca>
2012-08-21 19:07:41 -0400

    remove redis system envs

--- src/documeds/redis.clj
+++ src/documeds/redis.clj
@@ -3,10 +3,10 @@
 
 
 ; Redis Connection -----------------------------------------------
-(def redis-uri (get (System/getenv) "REDIS_URI"))
-(def redis-url (get (System/getenv) "REDIS_URL"))
-(def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
-(def redis-pass (get (System/getenv) "REDIS_PASS"))
+; (def redis-uri (get (System/getenv) "REDIS_URI"))
+; (def redis-url (get (System/getenv) "REDIS_URL"))
+; (def redis-port (Integer/parseInt (get (System/getenv) "REDIS_PORT")))
+; (def redis-pass (get (System/getenv) "REDIS_PASS"))
 
 (def r (delay (redis-client {:host "localhost", :port "6379"})))
 ; (def r (delay (redis-client {:host redis-url 
